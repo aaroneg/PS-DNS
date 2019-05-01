58e45ff6097756f7e2deac72ddaf4677b40d3ac1
Aaron <aaroneg@users.noreply.github.com>
2019-05-01 10:29:35 -0500

    Update 0-Del-test-zones.ps1

--- 0-Del-test-zones.ps1
+++ 0-Del-test-zones.ps1
@@ -1,5 +1,9 @@
 # Nuke test data and start over.
-$server1="lab-dns1"
 
+# Set the names of the origin and target servers
+$server1="lab-dns1"
+$server2="lab-dns2"
+
+# Get the zones, nuke the zones.
 Get-DnsServerZone -ComputerName $server1 | Remove-DnsServerZone -ComputerName $server1 -Force
 Get-DnsServerZone -ComputerName $server2 | Remove-DnsServerZone -ComputerName $server2 -Force
